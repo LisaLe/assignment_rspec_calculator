ada228bb3656994ad70029c33f2235b858d27646
Lisa Leung <lisaleung2291@gmail.com>
2016-09-22 21:59:16 -0700

    initial commit

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,1 +1,12 @@
 # Your code here
+require "../lib/calculator"
+
+describe Calculator do
+   @calculator = Calculator.new
+   describe add do 
+    it "should add all of the elements" do
+      expect(@calculator.add(1,2)).to eq(3)
+        
+    end
+   end
+end
